a55674d8346255cb4cd72d4a2572caf1ffbeb211
Muhammad Hussain Malik <98271133+hussain848586@users.noreply.github.com>
2023-07-14 22:29:37 +0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class Application {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JRadioButton MALERadioButton;
+    private JRadioButton FEMALERadioButton;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField streetTextField;
+    private JTextField cityTextField;
+    private JTextField addressTextField;
+    private JComboBox comboBox4;
+    private JComboBox comboBox5;
+    private JTextField PERCENTAGETextField;
+    private JTextField TOTALMARKSTextField;
+    private JTextField OBTAINMARKSTextField;
+    private JTextField PERCENTAGETextField1;
+    private JTextField TOTALMARKSTextField1;
+    private JTextField OBTAINMARKSTextField1;
+    private JTextField OBTAINMARKSTextField2;
+    private JTextField TOTALMARKSTextField2;
+    private JTextField PERCENTAGETextField2;
+    private JButton SUBMITButton;
+}
